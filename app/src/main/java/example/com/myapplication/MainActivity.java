7643620d9eee60dcc8a6c69d3e47a075520a4128
livelyattit <affan_94@hotmail.com>
2018-02-28 18:14:24 +0500

    Sample Data with object Collections Sorting and then iterate on it with for loop

--- app/src/main/java/example/com/myapplication/MainActivity.java
+++ app/src/main/java/example/com/myapplication/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import example.com.myapplication.model.DataItem;
+import example.com.myapplication.sample.SampleDataProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvout;
+    List<DataItem> dataItemList  = SampleDataProvider.dataItemList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         tvout = (TextView) findViewById(R.id.out);
 
-        DataItem item = new DataItem(null,"My menu Item", "a category","a description" , 1 , 9.95, "apple_pie.jpg");
-
-        tvout.setText(item.toString());
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+        for(DataItem item: dataItemList)
+        {
+             tvout.append(item.getItemName() + "\n");
+        }
 
     }
 }
