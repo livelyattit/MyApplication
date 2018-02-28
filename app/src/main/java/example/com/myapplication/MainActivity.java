5f3d9dbf9a445c326f091ad2b69fa9ebc21df344
livelyattit <affan_94@hotmail.com>
2018-02-28 18:39:53 +0500

    Sample Data with List View and default array adapter

--- app/src/main/java/example/com/myapplication/MainActivity.java
+++ app/src/main/java/example/com/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package example.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,25 +16,29 @@ import example.com.myapplication.sample.SampleDataProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvout;
+
     List<DataItem> dataItemList  = SampleDataProvider.dataItemList;
+    List<String> itemNames = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvout = (TextView) findViewById(R.id.out);
+     for(DataItem item: dataItemList)
+     {
+         itemNames.add(item.getItemName());
+     }
 
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
-            @Override
-            public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName().compareTo(o2.getItemName());
-            }
-        });
-        for(DataItem item: dataItemList)
-        {
-             tvout.append(item.getItemName() + "\n");
-        }
+     Collections.sort(itemNames);
+
+        ListView listView = (ListView) findViewById(android.R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1 , itemNames);
+
+        listView.setAdapter(adapter);
+
+
+
 
     }
 }
